607e43d2a2400cb17758b71d150dfcdb4334c31c
Ellis Percival <flyte@failcode.co.uk>
2021-12-25 12:56:08 +0000

    Update to Python3. Change tool.py to __main__.py.

--- christmas_elf/__main__.py
+++ christmas_elf/__main__.py
@@ -4,7 +4,7 @@ presents = {}
 def get_a_number(msg, first=True):
     if not first:
         msg += " (input must be a number) "
-    inpt = raw_input(msg)
+    inpt = input(msg)
     try:
         return int(inpt)
     except ValueError:
@@ -14,26 +14,28 @@ def get_a_number(msg, first=True):
 if __name__ == "__main__":
     # Ask the user to add recipients
     while True:
-        inpt = raw_input("Enter the name of a recipient or press Enter when done: ")
+        inpt = input("Enter the name of a recipient or press Enter when done: ")
         if not inpt:
             break
         presents[inpt] = {"counter": 0}
 
     # For each recipient, ask how many presents they have
     for recipient in presents:
-        presents[recipient]["amount"] = get_a_number("How many presents does %s have? " % recipient)
+        presents[recipient]["amount"] = get_a_number(
+            "How many presents does %s have? " % recipient
+        )
 
     ts = {}
     total_presents = sum(recipient["amount"] for recipient in presents.values())
     for i, person in enumerate(presents):
         step = float(total_presents) / presents[person]["amount"]
-        for j in range(1, presents[person]["amount"]+1):
-            ts[float("%s%s" % (step*j, i+1))] = person
+        for j in range(1, presents[person]["amount"] + 1):
+            ts[float("%s%s" % (step * j, i + 1))] = person
     for score, name in sorted(ts.items(), key=lambda x: x[0]):
-        print name
+        print(name)
     given = 0
     for score, name in sorted(ts.items(), key=lambda x: x[0]):
         given += 1
-        raw_input("Give %s a present! (%d remaining)" % (name, total_presents-given))
+        input("Give %s a present! (%d remaining)" % (name, total_presents - given))
 
-    print "All presents presented!"
+    print("All presents presented!")
